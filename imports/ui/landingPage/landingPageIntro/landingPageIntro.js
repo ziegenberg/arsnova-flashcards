0eedaae079318bfab5924b0e2238c3d0288cfd9a
Lindner <marlon.lindner@mni.thm.de>
2020-06-14 21:00:07 +0200

    finished navbar

--- imports/ui/landingPage/landingPageIntro/landingPageIntro.js
+++ imports/ui/landingPage/landingPageIntro/landingPageIntro.js
@@ -1,1 +1,54 @@
 import './landingPageIntro.html';
+
+Template.landingPageIntro.events({
+    "click .close": function () {
+        let modal = document.getElementById("NavPopupModal");
+        modal.style.display = "none";
+    },
+    "click #scrollImpressum": function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#landing-page-footer").offset().top
+		}, 600);
+        let modal = document.getElementById("NavPopupModal");
+        modal.style.display = "none";
+    },
+    "click #scrollQuestions": function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+			scrollTop: $("#landing-page-questions").offset().top
+		}, 600);
+        let modal = document.getElementById("NavPopupModal");
+        modal.style.display = "none";
+        },
+    "click #scrollFeatures": function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#landing-page-feature").offset().top + 51
+		}, 600);
+        let modal = document.getElementById("NavPopupModal");
+        modal.style.display = "none";
+        },
+    "click #scrollIntegrations": function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#landing-page-integrates").offset().top
+		}, 600);
+        let modal = document.getElementById("NavPopupModal");
+        modal.style.display = "none";
+        },
+    "click #scrollIntro": function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 600);
+        let modal = document.getElementById("NavPopupModal");
+        modal.style.display = "none";
+    },
+    "click": function (event) {
+        let modal = document.getElementById("NavPopupModal");
+        if (event.target === modal) {
+        	modal.style.display = "none";
+        }
+    }
+});
--- imports/ui/landingPage/landingPageNav/landingPageNav.js
+++ imports/ui/landingPage/landingPageNav/landingPageNav.js
@@ -1,1 +1,38 @@
 import './landingPageNav.html';
+
+Template.landingPageNav.events({
+	"click #navbar-icon": function () {
+	    let modal = document.getElementById("NavPopupModal");
+	    modal.style.display = "block";
+	},
+	"click #navScrollImpressum": function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#landing-page-footer").offset().top
+        }, 600);
+    },
+	"click #navScrollQuestions": function (event) {
+	    event.preventDefault();
+	    $('html, body').animate({
+            scrollTop: $("#landing-page-questions").offset().top
+        }, 600);
+	},
+	"click #navScrollFeatures": function (event) {
+	    event.preventDefault();
+	    $('html, body').animate({
+            scrollTop: $("#landing-page-feature").offset().top
+        }, 600);
+	},
+	"click #navScrollIntegrations": function (event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: $("#landing-page-integrates").offset().top
+		}, 600);
+	},
+	"click #navScrollIntro": function (event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: $("#landing-page-intro").offset().top
+		}, 600);
+	}
+});
