5d144d012a7a2d48463bb68cfc931530316d5894
Lindner <marlon.lindner@mni.thm.de>
2020-06-22 12:10:40 +0200

    added back to top button

--- imports/ui/landingPage/landingPageQuestions/landingPageQuestions.js
+++ imports/ui/landingPage/landingPageQuestions/landingPageQuestions.js
@@ -1,1 +1,13 @@
 import "./landingPageQuestions.html";
+import {toggleClass, animationTime} from "../landingPageNav/landingPageNav";
+
+Template.landingPageQuestions.events({
+	"click .landing-page-questions-backToTop i": function (event) {
+		event.preventDefault();
+		toggleClass(false);
+		$('html, body').animate({
+			scrollTop: 0
+		}, animationTime);
+		toggleClass(true);
+	}
+});
